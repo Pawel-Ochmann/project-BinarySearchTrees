e8e902bff73ff8df3eaf6ffba93ba46e91909400
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-07-09 14:16:06 +0200

    First commit, try to understand what the goal actually is

---
+++ main.js
@@ -0,0 +1,25 @@
+const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    constructor(array) {
+        this.root = this.buildTree(array);
+    }
+
+    buildTree(array) {
+
+    }
+    insert() {
+
+    }
+    delete() {
+        
+    }
+}
