f679e61e5a591e75f7b27bca2c82af26454888ae
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-07-09 15:16:02 +0200

    Add a function that sort array (using merge sort algorithm created in previous project) then removes duplicated elements

--- main.js
+++ main.js
@@ -1,25 +1,60 @@
-const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+function mergeSort(arr) {
+  function splitFirst(arr) {
+    if (arr.length === 1) return arr;
+    const firstHalf = arr.slice(0, Math.round(arr.length / 2));
+    return mergeSort(firstHalf);
+  }
+
+  function splitSecond(arr) {
+    if (arr.length === 1) return arr;
+    const secondHalf = arr.slice(Math.round(arr.length / 2));
+    return mergeSort(secondHalf);
+  }
+  function merge(a, b) {
+    let arraySorted = [];
+    while (a.length > 0 || b.length > 0) {
+      if (a[0] <= b[0] || b[0] === undefined) {
+        arraySorted.push(a.shift());
+      } else arraySorted.push(b.shift());
+    }
+    return arraySorted;
+  }
+
+  return merge(splitFirst(arr), splitSecond(arr));
+}
+
+function mySort(arr) {
+  arrSorted = mergeSort(arr);
+  const arrayMapped = [];
+  arrSorted.forEach((element) => {
+    if (element !== arrayMapped[arrayMapped.length - 1]) {
+      arrayMapped.push(element);
+    }
+  });
+  return arrayMapped;
+}
+
+const testArray = [
+  1, 7, 4, 23, 8, 1, 2, 4, 4, 67, 9, 3, 3, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345,
+  324,
+];
 
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 class Tree {
-    constructor(array) {
-        this.root = this.buildTree(array);
-    }
+  constructor(array) {
+    this.root = this.buildTree(array);
+  }
 
-    buildTree(array) {
-
-    }
-    insert() {
-
-    }
-    delete() {
-        
-    }
+  buildTree(array) {}
+  insert() {}
+  delete() {}
 }
+
+console.log(mySort(testArray));
